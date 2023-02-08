30cdf8b29f0cb6520ce9a286dd392394b8cdaa2f
jusroy <jroy1@laurentian.ca>
2023-02-08 10:15:29 -0500

    Create Assignment1_JH_JR.py

---
+++ Assignment1_JH_JR.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  6 21:03:13 2023
+
+@author: Justin Roy and James Humpfrey
+"""
+
+import PySimpleGUI as sg
+
+text = """Welcome to Pet Lyfe! This is an \"expert system\"  that will ask you a
+series of questions to estimate what kind of pet fits your lifestyle best.
+Select the different tabs and answer all questions to find out what kind
+of pet best suits your lifestyle."""
+
+column = [
+    [sg.Text(text, size=(60,4), justification="center")],
+    [
+    sg.Text("What age range best describes you?"),
+    sg.Radio("Under 20", "Group1"),
+    sg.Radio("20 - 29", "Group1"),
+    sg.Radio("30 - 39", "Group1"),
+    sg.Radio("40 - 49", "Group1"),
+    sg.Radio("50+", "Group1")
+    ],
+    [sg.Button("Exit")]]
+
+intro_layout = [[sg.VPush()],
+         [sg.Push(), sg.Column(column, element_justification='c'), sg.Push()],
+         [sg.VPush()]]
+
+window = sg.Window('Pet Lyfe', intro_layout)
+
+while True:
+    event, values = window.read()
+    
+    if event == "Exit" or event ==  sg.WIN_CLOSED:
+        break
+    
+window.close()
