a0e25b47be8564eeac0d72090a5b616ad1c16eab
jusroy <jroy1@laurentian.ca>
2023-02-08 19:58:57 -0500

    Update Assignment1_JH_JR.py

--- Assignment1_JH_JR.py
+++ Assignment1_JH_JR.py
@@ -15,42 +15,35 @@ of pet best suits your lifestyle."""
 column = [
     [sg.Text(text, size=(60,4), justification="center")],
     [
-    sg.Text("First of all, what is your prefered pet?"),
-    sg.Radio("Dog", "Group0"),
-    sg.Radio("Cat", "Group0"),
-    sg.Radio("Other", "Group0"),
-    sg.Radio("No preference", "Group0")
-    ],
-    [
-    sg.Text("What age range best describes you?"),
-    sg.Radio("Under 20", "Group1"),
-    sg.Radio("20 - 29", "Group1"),
-    sg.Radio("30 - 39", "Group1"),
-    sg.Radio("40 - 49", "Group1"),
-    sg.Radio("50+", "Group1")
-    ],
-    [
     sg.Text("What range of price are you willing to spend for the pet?"),
-    sg.Radio("Under 100$", "Group2"),
-    sg.Radio("100$ - 400$", "Group2"),
-    sg.Radio("401$ - 700$", "Group2"),
+    sg.Radio("Under 100$", "Group1"),
+    sg.Radio("100$ - 400$", "Group1"),
+    sg.Radio("401$ - 700$", "Group1"),
     sg.Radio("700$+", "Group2")
     ],
     [
     sg.Text("On a scale from 1 to 5, how active would you say you are?"),
-    sg.Radio("1", "Group3"),
-    sg.Radio("2", "Group3"),
-    sg.Radio("3", "Group3"),
-    sg.Radio("4", "Group3"),
-    sg.Radio("5", "Group3")
+    sg.Radio("1", "Group2"),
+    sg.Radio("2", "Group2"),
+    sg.Radio("3", "Group2"),
+    sg.Radio("4", "Group2"),
+    sg.Radio("5", "Group2")
     ],
     [
     sg.Text("How many hours per day are you away from home, for work or other reasons?"),
-    sg.Radio("1-2 hours", "Group4"),
-    sg.Radio("2-3 hours", "Group4"),
-    sg.Radio("3-5 hours", "Group4"),
-    sg.Radio("5-8 hours", "Group4"),
-    sg.Radio("8+ hours", "Group4")
+    sg.Radio("1-2 hours", "Group3"),
+    sg.Radio("2-3 hours", "Group3"),
+    sg.Radio("3-5 hours", "Group3"),
+    sg.Radio("5-8 hours", "Group3"),
+    sg.Radio("8+ hours", "Group3")
+    ],
+    [
+    sg.Text("How many hours per week are you willing to spend on training with your pet?"),
+    sg.Radio("1-2 hours", "Group3"),
+    sg.Radio("2-3 hours", "Group3"),
+    sg.Radio("3-4 hours", "Group3"),
+    sg.Radio("4-5 hours", "Group3"),
+    sg.Radio("5+ hours", "Group3")
     ],
     [sg.Button("Exit")]]
 
